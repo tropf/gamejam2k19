35854c7b6c17e9da13f43f3cfdc175dcea677c13
Rolf Pfeffertal <tropf@users.noreply.github.com>
2019-12-01 10:31:20 +0100

    new music, better endscreens

--- scenes/endscreen.gd
+++ scenes/endscreen.gd
@@ -6,12 +6,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if Global.peer:
+		Global.peer.close_connection()
+		Global.peer = null
 	if Global.won:
-		$wonscreen.show()
-		$lossscreen.queue_free()
+		get_tree().change_scene_to(preload("res://scenes/wonscreen.tscn"))
 	else:
-		$lossscreen.show()
-		$wonscreen.queue_free()
+		get_tree().change_scene_to(preload("res://scenes/lossscreen.tscn"))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
