f40cb97f1d2d92e27c09eaf3e77e53815b3b4549
Rolf Pfeffertal <tropf@users.noreply.github.com>
2019-12-01 14:07:48 +0100

    add new powerup display

--- scenes/HUD.gd
+++ scenes/HUD.gd
@@ -6,6 +6,7 @@ extends Control
 
 var score = Vector2(0, 0)
 var max_score = 17
+var powerup_markers = []
 
 signal max_score_reached
 
@@ -24,7 +25,21 @@ func change_score(playerdiff = 0, enemydiff = 0):
 		emit_signal("max_score_reached")
 
 func set_powerup_stop_count(curr):
-	$GridContainer/TextureRect/PowerUPstop.set_text("%s" % [curr])
+	var start_pos = $powerup_pos_marker.position
+	var delta_pos = Vector2(56, 0)
+	while curr > powerup_markers.size():
+		# spawn new markers
+		var new_marker = preload("res://scenes/powerup_stop.tscn").instance()
+		new_marker.set_pause_mode(PAUSE_MODE_STOP)
+		new_marker.position = start_pos + powerup_markers.size() * delta_pos
+		powerup_markers.append(new_marker)
+		add_child(new_marker)
+		
+	while curr < powerup_markers.size():
+		# delete markers
+		var last = powerup_markers.back()
+		last.queue_free()
+		powerup_markers.remove(powerup_markers.size() - 1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
