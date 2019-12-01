99b17ee7eb52171fd64cbb5c33d4b9a94001658b
Rolf Pfeffertal <tropf@users.noreply.github.com>
2019-12-01 05:40:26 +0100

    add music & sounds

--- scenes/countdown.gd
+++ scenes/countdown.gd
@@ -7,8 +7,15 @@ extends Control
 var init_text = "Ready!"
 var number_init = 4
 var number = number_init
+var sound = true
 
 signal done
+
+func _ready():
+	$beep_low.stream.loop = false
+	$beep_low.volume_db = -20
+	$beep_high.stream.loop = false
+	$beep_high.volume_db = -17
 
 func _on_Timer_timeout():
 	number -= 1
@@ -22,7 +29,11 @@ func start():
 func update_status():
 	$Label.text = str(number)
 	if number > 0:
+		if sound:
+			$beep_low.play()
 		$Timer.start()
 	else:
+		if sound:
+			$beep_high.play()
 		$Timer.stop()
 		emit_signal("done")
